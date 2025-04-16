b2aad6dd3986756b6701fdc68af5b4865369c3f2
unknown <user@DA536-A58.vtc.hk>
2025-04-16 12:32:49 +0800

    update login page

--- cinema_front_end/src/view/login.vue
+++ cinema_front_end/src/view/login.vue
@@ -1,83 +1,155 @@
 <template>
-  <a-form
-    :model="formState"
-    name="normal_login"
-    class="login-form"
-    @finish="onFinish"
-    @finishFailed="onFinishFailed"
-  >
-    <a-form-item
-      label="Username"
-      name="username"
-      :rules="[{ required: true, message: 'Please input your username!' }]"
-    >
-      <a-input v-model:value="formState.username">
-        <template #prefix>
-          <UserOutlined class="site-form-item-icon" />
-        </template>
-      </a-input>
-    </a-form-item>
+  <Row class="ml-10 mr-10">
+    <Col :span="12" class="overflow-hidden">
+      <img
+        class="imgcss"
+        src="https://mediafiles.cinema.com.hk/broadway/cmsimg/cinweb/webcms/images/4de53d9a3e9e189067c21a0d6a79e5de_1679394119.jpg"
+        alt=""
+      />
+    </Col>
+    <Col :span="8" :offset="2" align="center">
+      <div class="flex justify-between mt-20 mb-5 mr-26 ml-26">
+        <a-button
+          :type="bt1act ? 'primary' : 'default'"
+          @click="switchLoginMode(1)"
+          >会员登陆1</a-button
+        >
+        <a-button
+          :type="bt2act ? 'primary' : 'default'"
+          @click="switchLoginMode(2)"
+          >会员登陆2</a-button
+        >
+        <a-button
+          :type="bt3act ? 'primary' : 'default'"
+          @click="switchLoginMode(3)"
+          >会员登陆3</a-button
+        >
+      </div>
+      <a-form
+        :model="formState"
+        name="normal_login"
+        class="login-form"
+        @finish="onFinish"
+        @finishFailed="onFinishFailed"
+      >
+        <a-form-item
+          name="username"
+          :rules="[{ required: true, message: 'Please input your username!' }]"
+        >
+          <a-input
+            v-model:value="formState.username"
+            placeholder="用户名称/电子邮件"
+          >
+            <template #prefix>
+              <UserOutlined class="site-form-item-icon" />
+            </template>
+          </a-input>
+        </a-form-item>
 
-    <a-form-item
-      label="Password"
-      name="password"
-      :rules="[{ required: true, message: 'Please input your password!' }]"
-    >
-      <a-input-password v-model:value="formState.password">
-        <template #prefix>
-          <LockOutlined class="site-form-item-icon" />
-        </template>
-      </a-input-password>
-    </a-form-item>
+        <a-form-item
+          name="password"
+          :rules="[{ required: true, message: 'Please input your password!' }]"
+        >
+          <a-input-password
+            v-model:value="formState.password"
+            placeholder="密码"
+          >
+            <template #prefix>
+              <LockOutlined class="site-form-item-icon" />
+            </template>
+          </a-input-password>
+        </a-form-item>
+        <a href="#">忘记密码</a>
 
-    <a-form-item>
-      <a-form-item name="remember" no-style>
-        <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
-      </a-form-item>
-      <a class="login-form-forgot" href="">Forgot password</a>
-    </a-form-item>
+        <a-form-item class="mt-6">
+          <a-button
+            type="primary"
+            html-type="submit"
+            class="login-form-button mr-5"
+          >
+            重置
+          </a-button>
+          <a-button
+            :disabled="disabled"
+            type="primary"
+            html-type="submit"
+            class="login-form-button ml-5"
+          >
+            登入
+          </a-button>
+        </a-form-item>
 
-    <a-form-item>
-      <a-button :disabled="disabled" type="primary" html-type="submit" class="login-form-button">
-        Log in
-      </a-button>
-      Or
-      <a href="">register now!</a>
-    </a-form-item>
-  </a-form>
+        <a-form-item>
+          <a-form-item name="remember" no-style>
+            <a-checkbox v-model:checked="formState.remember"
+              >Remember me</a-checkbox
+            >
+          </a-form-item>
+          <a class="login-form-forgot" href="">Forgot password</a>
+        </a-form-item>
+      </a-form>
+    </Col>
+  </Row>
 </template>
 <script lang="ts" setup>
-import { reactive, computed } from 'vue';
-import { UserOutlined, LockOutlined } from '@ant-design/icons-vue';
-interface FormState {
-  username: string;
-  password: string;
-  remember: boolean;
-}
-const formState = reactive<FormState>({
-  username: '',
-  password: '',
-  remember: true,
-});
-const onFinish = (values: any) => {
-  console.log('Success:', values);
-};
+  import { reactive, computed } from 'vue'
+  import { UserOutlined, LockOutlined } from '@ant-design/icons-vue'
+  import { Row, Col } from 'ant-design-vue'
+  import { ref } from 'vue'
 
-const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
-};
-const disabled = computed(() => {
-  return !(formState.username && formState.password);
-});
+  const bt1act = ref(true)
+  const bt2act = ref(false)
+  const bt3act = ref(false)
+
+  const resetBtn = () => {
+    bt1act.value = false
+    bt3act.value = false
+    bt2act.value = false
+  }
+
+  const switchLoginMode = (mode: number) => {
+    resetBtn()
+    if (mode === 1) {
+      bt1act.value = true
+    } else if (mode === 2) {
+      bt2act.value = true
+    } else {
+      bt3act.value = true
+    }
+  }
+
+  interface FormState {
+    username: string
+    password: string
+    remember: boolean
+  }
+  const formState = reactive<FormState>({
+    username: '',
+    password: '',
+    remember: true,
+  })
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+  const disabled = computed(() => {
+    return !(formState.username && formState.password)
+  })
 </script>
 <style scoped>
-#components-form-demo-normal-login .login-form {
-  max-width: 300px;
-}
-#components-form-demo-normal-login .login-form-forgot {
-  float: right;
-}
-#components-form-demo-normal-login .login-form-button {
-  width: 100%;
-}
+  #components-form-demo-normal-login .login-form {
+    max-width: 300px;
+  }
+  #components-form-demo-normal-login .login-form-forgot {
+    float: right;
+  }
+  #components-form-demo-normal-login .login-form-button {
+    width: 100%;
+  }
+  .imgcss {
+    width: 100%;
+  }
 </style>
